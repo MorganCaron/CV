de0418a962cd66e1be15a558acabadae6e4966e1
Morgan <caron.morgan91@gmail.com>
2021-09-05 12:58:07 +0200

    Update CV

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,9 @@ module.exports = (env, argv) => {
 	return webpackConfigGenerator({
 		mode: argv.mode,
 		entry: {
-			app: ["./src/ts/App.ts", "./src/sass/style.sass", "./src/css/style.css"]
+			app: ["./src/ts/App.ts", "./src/sass/style.sass"]
 		},
+		index: "./src/index.html",
 		favicon: "./src/favicon.png"
 	});
 };
